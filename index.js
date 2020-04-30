b15546e6eccac8e1f8e63b2c8970e2c631a1373d
Midnight <rmargve@gmail.com>
2020-04-30 20:21:09 +0400

    added 404 route handling, added nodemon for development

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 3000,
+    host: 'localhost',
+    debug: { request: '*', log: '*' },
+  });
+
+  server.route({
+    method: '*',
+    path: '/{any*}',
+    handler: () => '404 Error! Page not found',
+  });
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => 'hello world',
+  });
+  await server.start();
+  server.log('info', `server running on ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.log('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
+init();
